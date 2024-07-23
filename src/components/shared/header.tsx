6f9fe8551fcf7900f84258f7cf99cfbc2de37a20
Vladimir <djbobah@mail.ru>
2024-07-23 17:03:51 +0300

    continue 39:56

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -15,6 +15,9 @@ export const Header: React.FC<HeaderProps> = ({ className }) => {
           <Image src="/logo.png" alt="Logo" width={35} height={35} />
           <div>
             <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+            <p className="text-sm text-gray-400 leading-3">
+              вкусней уже некуда
+            </p>
           </div>
         </div>
       </Container>
