7b805a8635234d3e7fe1fcb6ca18bb1696005630
Vladimir <djbobah@mail.ru>
2024-07-23 17:02:00 +0300

    continue

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,10 +1,23 @@
 import { cn } from "@/lib/utils";
 import React from "react";
+import { Container } from "./container";
+import Image from "next/image";
 
 interface HeaderProps {
   className: string;
 }
 
 export const Header: React.FC<HeaderProps> = ({ className }) => {
-  return <header className={cn("border border-b", className)}></header>;
+  return (
+    <header className={cn("border border-b", className)}>
+      <Container className="flex items-center justify-between py-8">
+        <div>
+          <Image src="/logo.png" alt="Logo" width={35} height={35} />
+          <div>
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
 };
