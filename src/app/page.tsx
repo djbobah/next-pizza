274143b6502f7a5d0bc39f36739aaa8eb37dcee3
Vladimir <djbobah@mail.ru>
2024-07-26 16:00:57 +0300

    1:06

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import { Button } from "@/components/ui/button";
+import {
+  Categories,
+  Container,
+  SortPopup,
+  Title,
+  TopBar,
+} from "@/components/shared";
 
 export default function Home() {
   return (
-    <h1>
-      Home <Button variant={"outline"}>rjhpbyf</Button>
-    </h1>
+    <>
+      <Container className="mt-5">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+
+      <Container className="pb-14">
+        <div className="flex gap-[60px]">
+          {/* фильтрация */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+          {/* список товаров */}
+          <div className="flex flex-col gap-16">
+            {/* <ProductsGroupList title="Пиццы" items={[1, 2, 3, 4, 5]} />
+            <ProductsGroupList title="Пиццы" items={[1, 2, 3, 4, 5]} /> */}
+            {/* список товаров */}
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
